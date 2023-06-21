a3e0f87f8ff0ab2380ec91715c2a0ccf9094c50d
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2023-06-21 16:21:55 +0200

    Adds 10 generated textfiles and the option to either use a random file or a specific one for RSVP

--- Assets/Scripts/Simple_RSVP.cs
+++ Assets/Scripts/Simple_RSVP.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
 using System.IO;
 using TMPro;
 
@@ -10,13 +11,30 @@ public class Simple_RSVP : MonoBehaviour
     public TMP_Text display;
     public int speed;
     public int startPause;
+    public string textFile;
+    public bool useRandomTextfile;
 
     private string[] inputArray;
     private float pauseInterval;
 
     private void Start()
     {
-        readTextFile("test.txt");
+        // Handle file selection either using a random file or a specific one
+        string directory = "Assets/TextFiles/";
+        string[] textFiles = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+        if(useRandomTextfile){
+            System.Random rnd = new System.Random();
+            int randomFile = rnd.Next(0, 9);
+            readTextFile(directory + textFiles[randomFile].ToString() + ".txt");
+
+            Debug.Log("Using random File " + textFiles[randomFile].ToString());
+
+        }else{
+            readTextFile(directory + textFile + ".txt");
+            
+            Debug.Log("Using File " + textFile);
+        }
 
         // Convert words per minute into seconds between words
         pauseInterval = 1/(speed/60f);
@@ -29,10 +47,12 @@ public class Simple_RSVP : MonoBehaviour
     }
 
     IEnumerator  RSVP_Display(){
+        // Specify active phase before starting wait-timer
         DataScript.Phase = "calibration";
         // Wait before start
         yield return new WaitForSeconds(startPause);
 
+        // Specify active phase before starting RSVP
         DataScript.Phase = "test";
 
         foreach (var word in inputArray)

a4b6d8cd1cad9485bb02449a4ed3ddf051e7ac79
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2023-07-03 18:08:11 +0200

    Removes unneccesary Options from Manager: - Background Color is always Grey - Calculation Method is always Average
    Makes NormalReading always choose the same text file and generate a new Random Order
    
    Moves txt files from pilot study in new folder

--- Assets/Scripts/Normal_Reading.cs
+++ Assets/Scripts/Normal_Reading.cs
@@ -10,9 +10,7 @@ public class Normal_Reading : MonoBehaviour
 {
     public TMP_Text display;
     public int startPause;
-    public bool generateNewRandomOrder;
 
-    private string activeTextFile;
     private string directory;
 
     // Start is called before the first frame update
@@ -23,18 +21,12 @@ public class Normal_Reading : MonoBehaviour
         directory = "Assets/TextFiles/";
         string randomOrderFile = directory + "RandomOrder.txt";
 
-        if (generateNewRandomOrder)
-        {
-            string[] textFiles = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l" };
+            string[] textFiles = new string[] { "a", "b", "c", "d", "e", "f", "g", "h"};
 
             ShuffleArray(textFiles);
 
-            Debug.Log("Random Order generated! Starting with file " + textFiles[0]);
-            activeTextFile = textFiles[0];
-            DataScript.ActiveTextFile = activeTextFile;
-
-            // Delete the first Item that has already been used
-            DeleteFirstItem(ref textFiles);
+            Debug.Log("Random Order generated!");
+            DataScript.ActiveTextFile = "NormalReading";
 
             // Clear the txt file from previous uses
             using (FileStream fileStream = new FileStream(randomOrderFile, FileMode.Truncate))
@@ -51,48 +43,7 @@ public class Normal_Reading : MonoBehaviour
                     writer.WriteLine(file);
                 }
             }
-        }
-        else
-        {
-            // Get the random Order from the txt file and convert it to an array while removing initial linebreaks or whitespaces
-            string randomOrder = readFile(randomOrderFile).Trim();
-            char[] randomOrderArray = randomOrder.ToCharArray();
-
-            if (randomOrderArray.Length != 0)
-            {
-                // Specify the used thext file for CSV export
-                activeTextFile = randomOrderArray[0].ToString();
-                DataScript.ActiveTextFile = activeTextFile;
-
-                string[] inputArray = readFile(directory + activeTextFile + ".txt").Split(' ');
-                Debug.Log("Using File " + activeTextFile);
-
-                // Delete the first item in the array that has just been "used"
-                DeleteFirstItem(ref randomOrderArray);
-
-                // Clear the txt file from previous uses
-                using (FileStream fileStream = new FileStream(randomOrderFile, FileMode.Truncate))
-                {
-                    // Set the length of the file stream to 0 to clear its contents
-                    fileStream.SetLength(0);
-                }
-
-                // Write the randomly generated order into a text file with one less file name
-                using (StreamWriter writer = new StreamWriter(randomOrderFile))
-                {
-                    foreach (var file in randomOrderArray)
-                    {
-                        writer.Write(file);
-                    }
-                }
-
-                StartCoroutine(Display());
-            }
-            else
-            {
-                Debug.Log("No more files left!");
-            }
-        }
+        
         StartCoroutine(Display());
     }
 
@@ -115,13 +66,6 @@ public class Normal_Reading : MonoBehaviour
         array[j] = temp;
     }
 
-    static void DeleteFirstItem<T>(ref T[] array)
-    {
-        T[] newArray = new T[array.Length - 1];
-        Array.Copy(array, 1, newArray, 0, newArray.Length);
-        array = newArray;
-    }
-
     string readFile(string filePath)
     {
         using (StreamReader reader = new StreamReader(filePath))
@@ -142,7 +86,6 @@ public class Normal_Reading : MonoBehaviour
         // Specify active phase before starting RSVP
         DataScript.Phase = "test";
 
-        display.text = readFile(directory + activeTextFile + ".txt");
-
+        display.text = readFile(directory + "MainStudy_1/NormalReading.txt");
     }
 }
